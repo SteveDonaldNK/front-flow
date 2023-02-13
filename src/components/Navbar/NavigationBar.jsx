7f719b04f82f4f0fb81abfc1dd0a68487ff838fd
Sakpak Donald <sakpakd@gmail.com>
2023-02-13 22:51:31 +0100

    create offcanvas menu

--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Navbar, Nav, NavDropdown, Button, Offcanvas } from 'react-bootstrap'
 import Logo from '../../assets/logo.png';
 import './styles.css'
+import { useWindowSize } from 'react-use';
+
+const MenuBtn = () => (
+  <span className='toggler navbar-toggler-icon'></span>
+)
+
+const NavLinks = () => (
+  <Nav className='m-auto gap-5'>
+    <Nav.Link className='links' href='/'>Home</Nav.Link>
+    <NavDropdown className='links' title='Blog' id='dropdown-navigation' >
+      <NavDropdown.Item href='/'>Home</NavDropdown.Item>
+      <NavDropdown.Item href='/'>Blog</NavDropdown.Item>
+      <NavDropdown.Item href='/'>About us</NavDropdown.Item>
+    </NavDropdown>
+    <Nav.Link className='links' href='/'>About us</Nav.Link>
+    <Nav.Link className='links' href='/'>Contact us</Nav.Link>
+  </Nav>
+)
+
+const NavBtn = () => (
+  <div>
+    <Button className='nav-bar-btn px-3 py-1.6 white-btn' variant='light'><span>Log in</span></Button>
+    <Button className='nav-bar-btn px-3 py-1.6 ms-2' variant='dark'><span>Sign up</span></Button>
+  </div>
+)
 
 export default function NavigationBar() {
+  const { width } = useWindowSize();
+  const [show, setShow] = useState(false);
+  const bp = 991;
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
       <Navbar className='px-5 nav-bar' bg="light" expand="lg">
         <Navbar.Brand href='/' className='d-flex justify-content-center align-items-center gap-2 fw-bold'>
           <img className='logo' src={Logo} alt="Logo" /><span className='logo-label'>Front-flow</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='navigation'/>
-        <Navbar.Collapse id='navigation'>
-          <Nav className='m-auto gap-5'>
-            <Nav.Link className='links' href='/'>Home</Nav.Link>
-            <NavDropdown className='links' title='Blog' id='dropdown-navigation' >
-              <NavDropdown.Item href='/'>Home</NavDropdown.Item>
-              <NavDropdown.Item href='/'>Blog</NavDropdown.Item>
-              <NavDropdown.Item href='/'>About us</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className='links' href='/'>About us</Nav.Link>
-            <Nav.Link className='links' href='/'>Contact us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Button className='nav-bar-btn px-3 py-1.6 white-btn' variant='light'><span>Log in</span></Button>
-        <Button className='nav-bar-btn px-3 py-1.6 ms-2' variant='dark'><span>Sign up</span></Button>
+        <Navbar.Toggle className='border-0' onClick={handleShow}><MenuBtn  /></Navbar.Toggle>
+        { width > bp && <NavLinks/> }
+        { width > bp &&  <NavBtn />}
+        <Offcanvas show={show} onHide={handleClose} placement="end" responsive="lg" >
+          <Offcanvas.Body>
+            { width <= bp && <NavLinks/> }
+            { width <= bp && <NavBtn /> }
+          </Offcanvas.Body>
+        </Offcanvas>
       </Navbar>
   )
 }
