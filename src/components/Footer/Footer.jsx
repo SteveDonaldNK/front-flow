051e34e6985f2c5e45828f6dbf42c4de1eda66de
Sakpak Donald <sakpakd@gmail.com>
2023-02-22 14:35:22 +0100

    Update footer components' styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -65,14 +65,17 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   const {width} = useWindowSize();
   let size = 6;
+  let textDir, btnDir = 'horizontal';
   width <= 440 && (size = 12);
+  width <= 500 && (textDir = 'vertical');
+  width <= 300 && (btnDir = 'vertical');
 
   return (
     <Container className='mb-3 text-light footer' fluid>
       <Container className='text-center' fluid>
         <h1>Let's get started on something great</h1>
         <h5 className='mt-3'>Join over 4,000+ startups already growing with Front-flow.</h5>
-        <Stack direction='horizontal' gap={3} className='justify-content-center mt-5'>
+        <Stack direction={btnDir} gap={3} className='justify-content-center mt-5'>
           <Button variant='dark' className='border-light'>Chat to us</Button>
           <Button variant='dark' className='border-light'>Get started</Button>
         </Stack>
@@ -80,9 +83,9 @@ export default function Footer() {
       <Container fluid className='footer-link-container d-flex justify-content-between'>
         {width > 600 ? <FullFooterLinks /> : <AccordionLinks size={size} />}
       </Container>
-      <Stack className='px-5 mt-5 copy-right' direction='horizontal'>
+      <Stack className='px-5 mt-5 copy-right text-center' direction={textDir}>
         <a href="/"><img className='logo me-2' src={logo} alt="Logo" /><span>Front-flow</span></a>
-        <p className='ms-auto'>&copy; {currentYear} Front-flow. All rights reserved</p> 
+        <p className='copy-right-text ms-auto mb-0'>&copy; {currentYear} Front-flow. All rights reserved</p> 
       </Stack>
     </Container>
   )
