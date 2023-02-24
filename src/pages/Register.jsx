d171c7e366e1ea5b5af0e95b528abb795d27aba7
Sakpak Donald <sakpakd@gmail.com>
2023-02-24 19:58:22 +0100

    Update signup page id's and classes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import SignUp from '../components/Sign-up/SignUp'
 
 export default function Register() {
   return (
-    <div id='login-container'>
+    <div id='signup-container'>
         <SignUp /> 
     </div>
   )
