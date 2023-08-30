319bec8d077423f67d6342eadab0029706910c6d
Sakpak Donald <sakpakd@gmail.com>
2023-08-30 22:18:33 +0100

    add category bar

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -10,8 +10,12 @@ export default function BlogCard() {
         <Card.Img className='card-image' src={image} />
         <Card.Title>Migrating to linear 101</Card.Title>
         <Card.Body>Linear helps streamline software projects, sprints, tasks and bug tracking. Here's how to get started</Card.Body>
-        <Stack>
-            <Avatar size={50} />
+        <Stack className='avatar mt-4 align-center gap-3' direction='horizontal'>
+          <Avatar size={50} />
+          <div>
+            <strong>Phoenix Baker</strong>
+            <p className='fw-semibold m-0 text-secondary'>19 Jan 2022</p>
+          </div>
         </Stack>
     </Card>
   )
--- src/components/BlogView/BlogView.jsx
+++ src/components/BlogView/BlogView.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Col, Container, Row, Image, Stack, Card, Collapse} from 'react-bootstrap'
+import React, { useEffect, useRef, useState } from 'react'
+import { Col, Container, Row, Image, Stack, Card, Collapse, Button} from 'react-bootstrap'
 import image from '../../assets/image 1.jpg'
 import Avatar from '../../components/Avatar/Avatar'
 import BlogCard from '../BlogCard/BlogCard'
@@ -7,20 +7,51 @@ import VertLine from '../VertLine/VertLine'
 import PagePagination from '../PagePagination/PagePagination'
 import Line from '../Line/Line'
 import './styles.css'
+import { ChevronCompactLeft, ChevronCompactRight } from 'react-bootstrap-icons'
 
 export default function BlogView() {
+
+  const [visible, setVisible] = useState(false);
+  const tabGroupRef = useRef(null);
+  const hideTabStyle = 'translate(100%, -50%)';
+  const showTabStyle = 'translate(0, -50%)'
+
+  const handleShow = () => {
+    const tabStyleObj = tabGroupRef.current.style
+    visible ? tabStyleObj.transform = hideTabStyle : tabStyleObj.transform = showTabStyle;
+    setVisible(!visible)
+  }
+
+  const resetTabStyle = () => {
+    const tabStyleObj = tabGroupRef.current.style;
+    tabStyleObj.transform = showTabStyle;
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', resetTabStyle);
+  
+    return () => {
+      window.removeEventListener('resize', resetTabStyle);
+    }
+  }, [])
+  
+
   return (
     <Container className='mb-5 p-0' fluid>
         <Row className='m-0'>
-            <Col className='p-0' lg={9}>
+            <Col className='p-0' xxl={9} xl={9} lg={9} md={12} sm={12} xs={12}>
               <Row className='m-0'>
                 <Line color={'var(--secondary-color)'} />
                 <Container className='p-0 my-4' fluid>
                   <Image className='top-card-image float-start' src={image} />
                     <h3 className='fw-bold'>Will AI replace UX writing?</h3>
                     <p>Concerned with the idea that robots are comming to take your job? Here's what the data says.</p>
-                    <Stack className='avatar'>
+                    <Stack className='avatar align-center gap-3' direction='horizontal'>
                       <Avatar size={50} />
+                      <div>
+                        <strong>Phoenix Baker</strong>
+                        <p className='fw-semibold m-0 text-secondary'>19 Jan 2022</p>
+                      </div>
                     </Stack>
                 </Container>
                 <Line color={'var(--secondary-color)'} />
@@ -39,7 +70,13 @@ export default function BlogView() {
             </Col>
 
             <Col className='p-0' lg={3}>
-              <Container className='m-0 px-5'>
+              <Container ref={tabGroupRef} className='m-0 px-5 tab-container'>
+                <Button onClick={handleShow} className='position-absolute tab-btn' >
+                  {visible ?
+                    <ChevronCompactRight color={'var(--grey-color-xxl)'} size={32} /> :
+                    <ChevronCompactLeft color={'var(--grey-color-xxl)'} size={32} />
+                  }
+                </Button>
                 <ul className='tab-group'>
                   <li className='tab active'>View all</li>
                   <li className='tab'>Design</li>
