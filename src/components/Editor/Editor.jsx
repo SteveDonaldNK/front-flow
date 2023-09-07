dca6bd63b8ba76a09ecc6ba28cb0fe23208966fb
Sakpak Donald <sakpakd@gmail.com>
2023-09-07 23:21:38 +0100

    style compose page

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -3,34 +3,81 @@ import ReactQuill, { Quill } from 'react-quill'
 import ImageResize from 'quill-image-resize-module-react'
 import 'react-quill/dist/quill.snow.css';
 import './styles.css'
+import { Button, ButtonGroup, ButtonToolbar, Col, Container, FloatingLabel, Form, Image, Row, Stack } from 'react-bootstrap';
+import { FileUploader } from 'react-drag-drop-files';
 
 Quill.register('modules/imageResize', ImageResize)
 
 export default function Editor() {
     const [value, setValue] = useState('');
-    var toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    const fileTypes = ["JPG", "PNG", "webp"];
+    const [file, setFile] = useState(null);
+    const toolbarOptions = [
+        [{ 'size': ['small', false, 'large', 'huge'] },'bold', 'italic', 'underline', 'strike'],        // toggled buttons
         ['blockquote', 'code-block'],
-        ['link', 'image'],
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-      
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-      
-        ['clean']                                         // remove formatting button
-      ];
+        [{ 'list': 'bullet'}, { 'list': 'ordered' }, { 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'align': [] }, { 'color': [] }, { 'background': [] }],
+        ['link', 'image', 'clean'],
+    ];
+
+    const handleChange = (file) => {
+        setFile(file);
+        const url = URL.createObjectURL(file)
+    };
+    const handleRemove = () => {
+        setFile(null);
+    };
 
   return (
-    <ReactQuill className='editor' theme='snow' modules={{ toolbar: toolbarOptions, imageResize: {
-        parchment: Quill.import('parchment'),
-        modules: ['Resize', 'DisplaySize']
-     }}} value={value} onChange={setValue} />
+    <Container fluid>
+        <Row>
+            <Col xl={8} lg={12}>
+                <Form className='editor-form'>
+                    <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control className='mb-3' type="text" placeholder="Enter post title" />
+                        <Form.Label>Content</Form.Label>
+                        <ReactQuill className='editor' theme='snow' formats={[
+                            "header",
+                            "font",
+                            "size",
+                            "bold",
+                            "italic",
+                            "underline",
+                            "strike",
+                            "blockquote",
+                            "list",
+                            "bullet",
+                            "indent",
+                            "link",
+                            "image",
+                            "color"
+                            ]} modules={{ toolbar: toolbarOptions,
+                            clipboard: {
+                                matchVisual: false,
+                            }, imageResize: {
+                            parchment: Quill.import('parchment'),
+                            modules: ['Resize', 'DisplaySize']
+                        }}} value={value} onChange={setValue} />
+                    </Form.Group>
+                </Form>
+            </Col>
+            <Col xl={4} lg={12}>
+                <Container className='p-0 m-0' fluid>
+                    <Container className='featured-image-container px-4 py-5' fluid>
+                        <h5 className='fw-bold mb-4'>Featured image</h5>
+                        <Container className='m-0 p-0' fluid>
+                        {file ? <Image className='featured-image' src={URL.createObjectURL(file)} />:
+                            <FileUploader label="Upload or drop a file here" maxSize={15} handleChange={handleChange} name='file' types={fileTypes} />
+                        }
+                        </Container>
+                        <Container className='w-100 p-0 text-end mt-3'>
+                            <Button onClick={handleRemove} variant='outline-danger' className='m-auto'>Remove</Button>
+                        </Container>
+                    </Container>
+                </Container>
+            </Col>
+        </Row>
+    </Container>
   )
 }
--- src/pages/Compose.jsx
+++ src/pages/Compose.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import NavigationBar from '../components/NavigationBar/NavigationBar'
 import Editor from '../components/Editor/Editor'
+import { Container } from 'react-bootstrap'
 
 export default function Compose() {
   return (
     <>
-        <NavigationBar />
-        <Editor />
+      <NavigationBar />
+      <Container className='compose-container' fluid>
+          <Editor />
+      </Container>
     </>
   )
 }
